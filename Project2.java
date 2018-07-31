0059f0de7ea6a49c55be06e6dfe2c3a511ea9c6c
VKarGit <32172281+VKarGit@users.noreply.github.com>
2018-07-30 22:25:07 -0400

    a

--- Project2.java
+++ Project2.java
@@ -1,3 +1,7 @@
+package Project2;
+
+import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
+
 import java.util.Scanner; //Imports scanner
 
 public class Project2 {
@@ -7,13 +11,13 @@ public class Project2 {
         //Define/Initialize variables
         Deck deck = new Deck(); //Makes instance of deck class
 
-        Card player = new Card();  //Makes instance of card class for player
-
-        Card dealer = new Card(); //Makes instance of card class for player
+        Card card = new Card(); //Makes instance of card class
         int rounds = 0;
 
+        boolean nextLoop;
+
         String playerChoice;
-        String play = "Yes";
+        String play = "yes";
 
         System.out.println("You are playing black jack!");
         System.out.println("The goal of the game is get as close to 21 as possible without passing it.");
@@ -22,215 +26,217 @@ public class Project2 {
         System.out.println("      In that case, the value of the ace is 1.\n");
         System.out.println("Good luck!\n");
 
-
         do {
-            deck.fillDeck(); //Starts game with a full deck
 
+            deck.fillDeck(); //Starts game with a full deck  //////////////////////////////d
 
-            int[] playerCards = new int[52]; //Creates array to store all cards pulled by player
-            int playerCardCount = 2; //Keeps track of number of cards the player has
-
-            int[] dealerCards = new int[52]; //Creates array to store all cards pulled by player
-            int dealerCardCount = 2; //Keeps track of number of cards the player has
+            Hand playerHand = new Hand(); //Makes instance of hand class for player
+            Hand dealerHand = new Hand(); //Makes instance of hand class for dealer
 
             if (rounds > 0) { //Asks user if they would like to play the game again only after the first round
-                System.out.print("\nWould you like to play again?");
+                System.out.print("\nWould you like to play again? ");
                 play = input.nextLine();
-                System.out.print("");
 
                 if (play.equalsIgnoreCase("no")) {
-                    System.out.println("Thanks for playing!");
+                    System.out.println("\nThanks for playing!");
                     break;
                 }
             }
 
             System.out.println("Your first 2 cards are: ");
 
-            for (int j = 0; j < playerCardCount; j++) { //Loops two times(Gives player 2 cards)
-                player.setHandValue(playerCards); //Calculates the value of the cards the player has picked
-                //Loops until a card is found that hasn't been used
-                do {
-                    deck.setRandCard();
-                } while (deck.getRandCard().equals("R"));
-
-                playerCards[j] = player.getCardValue(deck.getRandCard(), player.getHandValue()); //Finding the value of the random card
-                System.out.println(deck.getRandCard()); //Prints the cards
-
-                deck.removeCard(deck.getRandCard()); //Removes card from the deck(replaces the String value with "R")
+            for (int j = 0; j < 2; j++) { //Picks first two cards for player
+                pickCard(playerHand, deck, card, true);
+                playerHand.setCardCount();
             }
 
+            System.out.println(playerHand.getHandValue()); //Prints the cards
 
-            player.setHandValue(playerCards); //Calculates the value of the cards the player has picked
-
-            if (player.getHandValue() == 21) { //Checks if the sum of the dealer's cards are 21
-                System.out.println("Congratulations! You won the game!");
-                rounds++;
-                continue;
-            } else if (player.getHandValue() > 21) {
-                System.out.println("You have busted!");
+            if (winOrBustPlayer(playerHand)) { //Checks if the player has won or busted
                 rounds++;
                 continue;
             }
 
+            System.out.println("\nOne of the dealers cards are: ");
 
-            System.out.println();
-            System.out.println("One of the dealers cards are: ");
-
-            for (int j = 0; j < dealerCardCount; j++) {
-                dealer.setHandValue(dealerCards); //Sets the value of the dealer's deck
-
-                //Loops until a card is found that hasn't been used
-                do {
-                    deck.setRandCard();
-                } while (deck.getRandCard().equals("R"));
-
+            for (int j = 0; j < 2; j++) {
                 if (j == 0) { //Only displays one of the dealer's cards
-                    System.out.println(deck.getRandCard());
-                    System.out.println("");
+                    pickCard(dealerHand, deck, card, true);
+                } else {
+                    pickCard(dealerHand, deck, card, true);///////////////////////////////////////////////////////
                 }
 
-                dealerCards[j] = dealer.getCardValue(deck.getRandCard(), dealer.getHandValue()); //Finding the point value of the random card
-
-                deck.removeCard(deck.getRandCard()); //Removes card from the deck(replaces the String value with "R")
+                dealerHand.setCardCount();
             }
-            dealer.setHandValue(dealerCards); //Calculates the value of the cards the dealer has picked
 
-            if (dealer.getHandValue() == 21) { //Checks if the sum of the dealer's cards are 21
-                System.out.println("The dealer has won.");
-                rounds++;
-                continue;
-            } else if (dealer.getHandValue() > 21) {
-                System.out.println("The dealer busted so you won!");
+            System.out.println(dealerHand.getHandValue()); //Prints the cards
+
+            if (winOrBustDealer(dealerHand)) {
                 rounds++;
                 continue;
             }
 
-            //hit/stay for player
             do {
+                nextLoop = false;
                 System.out.print("Would you like to hit or stay?");
                 playerChoice = input.nextLine();    //Creates variable from input
                 System.out.println();
 
                 if (playerChoice.equalsIgnoreCase("Hit")) { //If the user's choice is hit then the dealer(computer) gives the player another card
-                    playerCardCount++;
                     System.out.println("This is your new card: ");
 
-                    //Loops until a card is found that hasn't been used
-                    do {
-                        deck.setRandCard();
-                    } while (deck.getRandCard().equals("R"));
+                    pickCard(playerHand, deck, card, true);
+                    playerHand.setCardCount();
 
-                    playerCards[playerCardCount - 1] = player.getCardValue(deck.getRandCard(), player.getHandValue()); //finding the value of the random card
-                    System.out.println(deck.getRandCard() + "\n"); //Prints the new card
-
-                    deck.removeCard(deck.getRandCard()); //Removes card from the deck(replaces the String value with "R")
-
-                    player.setHandValue(playerCards); //Sets the total point value of the player's hand
-
-                    if (player.getHandValue() == 21) { //Checks if the sum of the dealer's cards are 21
-                        break;
-                    } else if (player.getHandValue() > 21) {
+                    if (winOrBustPlayer(playerHand)) {
+                        rounds++;
+                        nextLoop = true; //variable nextLoop lets the next round start since the player won or bust
                         break;
                     }
                 }
             } while (playerChoice.equalsIgnoreCase("Hit"));
-
-            player.setHandValue(playerCards); //Calculates the value of the cards the player has picked
-
-            if (player.getHandValue() == 21) { //Checks if the sum of the dealer's cards are 21
-                System.out.println("Congratulations! You won the game!");
-                rounds++;
-                continue;
-            } else if (player.getHandValue() > 21) {
-                System.out.println("You have busted!");
-                rounds++;
+             if (nextLoop) {
                 continue;
             }
 
             System.out.println("Now it's the dealer's turn!");
 
             //Computer/dealer's automated hit/stay logic
-            while (dealer.getHandValue() < 17) {
-                dealerCardCount++;
+            while (dealerHand.getHandValue() < 17) {
+                pickCard(dealerHand, deck, card, true); /////////////////////////////////////
+                dealerHand.setCardCount();
 
-                //Loops until a card is found that hasn't been used
-                do {
-                    deck.setRandCard();
-                } while (deck.getRandCard().equals("R"));
-
-                dealerCards[dealerCardCount - 1] = dealer.getCardValue(deck.getRandCard(), dealer.getHandValue()); //finding the value of the random card
-
-                deck.removeCard(deck.getRandCard()); //Removes card from the deck(replaces the String value with "R")
-
-                dealer.setHandValue(dealerCards); //Calculates the value of the cards the dealer has picked
-
-
-                if (dealer.getHandValue() == 21) { //Checks if the sum of the dealer's cards are 21
+                if (winOrBustDealer(dealerHand)) {
                     rounds++;
-                    break;
-                } else if (dealer.getHandValue() > 21) {
-                    rounds++;
+                    nextLoop = true; //variable nextLoop lets the next round start since the player won or bust
                     break;
                 }
             }
 
-            if (dealer.getHandValue() == 21) { //Checks if the sum of the dealer's cards are 21
-                System.out.println("The dealer has won.");
-                rounds++;
-                continue;
-            } else if (dealer.getHandValue() > 21) {
-                System.out.println("The dealer busted so you won!");
-                rounds++;
+            if (nextLoop) {
                 continue;
             }
 
-            //Tells if player or dealer won
-            player.setHandValue(playerCards);
-            dealer.setHandValue(dealerCards);
-
-            if (dealer.getHandValue() > player.getHandValue()) { //Finds if the dealer or the player is the winner
-                System.out.println("The dealer won the game with a total of " + dealer.getHandValue() + " while you had a total of " + player.getHandValue() + ".");
-            } else if (dealer.getHandValue() < player.getHandValue()) {
-                System.out.println("You won with a total of " + player.getHandValue() + " while the dealer had a total of " + dealer.getHandValue() + ".");
+            if (dealerHand.getHandValue() > playerHand.getHandValue()) { //Finds if the dealer or the player is the winner
+                System.out.println("The dealer won the game with a total of " + dealerHand.getHandValue() + " while you had a total of " + playerHand.getHandValue() + ".");
+            } else if (dealerHand.getHandValue() < playerHand.getHandValue()) {
+                System.out.println("You won with a total of " + playerHand.getHandValue() + " while the dealer had a total of " + dealerHand.getHandValue() + ".");
             } else {
-                System.out.println("Both you and the dealer tied with " + dealer.getHandValue() + " points.");
+                System.out.println("Both you and the dealer tied with " + dealerHand.getHandValue() + " points.");
             }
 
             rounds++;
         } while (play.equalsIgnoreCase("yes"));
     }
-}
 
-class Card {
-    private int handValue = 0;
-    private String cardRank;
-    
-    public int getCardValue(String cardVisual, int totalHandValue) { //Gets the value of the card
-        cardRank = cardVisual.substring(0, cardVisual.indexOf("-"));
-        if (cardRank.equals("J") || cardRank.equals("Q") || cardRank.equals("K")) { //Extracts rank
-            cardRank = "10";
-        } else if (cardRank.equals("A")) { //If the hand total plus 11 doesn't make the player/dealer bust, then the value of the ace is 11
-            if (totalHandValue + 11 <= 21) {
-                cardRank = "11";
-            } else {
-                cardRank = "1";
-            }
+    public static void pickCard(Hand hand, Deck deck, Card card, boolean printTrue) {
+        deck.setRandCardVisual();
+        card.setCardRank(deck.getRandCardVisual());
+        hand.addCard(hand.getCardValue(card.getCardRank()));
+        hand.setHandValue();
+        if (printTrue) {
+            System.out.println(deck.getRandCardVisual()); //Prints the cards
         }
+        deck.removeCard(deck.getRandCardVisual());
+    }
 
-        return Integer.valueOf(cardRank);
+    public static boolean winOrBustPlayer(Hand hand) {
+        hand.setHandValue();
+        if (hand.getHandValue() == 21) { //Checks if the sum of the dealer's cards are 21
+            System.out.println("Congratulations! You won the game!");
+            return true;
+        } else if (hand.getHandValue() > 21) {
+            System.out.println("You have busted!");
+            return true;
+        } else {
+            return false;
+        }
     }
 
 
-    public void setHandValue(int[] hand) { //Calculates the hand value
-        handValue = 0;
-        for (int j = 0; j < hand.length; j++) {
-            handValue += hand[j];
+    public static boolean winOrBustDealer(Hand hand) {
+        hand.setHandValue();
+        if (hand.getHandValue() == 21) { //Checks if the sum of the dealer's cards are 21
+            System.out.println("The dealer has won.");
+            return true;
+        } else if (hand.getHandValue() > 21) {
+            System.out.println("The dealer busted so you won!");
+            return true;
+        } else {
+            return false;
         }
+    }
+}
+
+class Hand {
+
+    int[] handArray = new int[52];
+    private int handValue = 0;
+    int cardCount = 0;
+    int cardValue = 0;
+
+    public void addCard(int card) {
+        handArray[cardCount + 1] = card;
+    }
+
+    public void setCardCount() {
+        cardCount++;
+    }
+
+    public int getCardCount() {  /////////////////////////////////////////////////
+        return cardCount;
+    }
+
+    public void setHandValue() { //Calculates the hand value
+        handValue = 0;
+        for (int j = 0; j < handArray.length; j++) {
+            handValue += handArray[j];
+        }
+
+
+        if (handValue > 21) { //Changes the value of all aces valuing to 11 and change it's value to 1 so the player/dealer doesn't bust
+            for (int j = 0; j < handArray.length; j++) {
+                if (handArray[j] == 11){
+                    handArray[j] = 1;
+                }
+                handValue += handArray[j];
+            }
+        }
+      //  System.out.println("handValue " + handValue);
     }
 
     public int getHandValue() { //Returns the hand value
         return handValue;
     }
+
+    public int getCardValue(String cardRank) { //Gets the value of the card
+        if (cardRank.equals("J") || cardRank.equals("Q") || cardRank.equals("K")) { //Extracts rank
+            cardValue = 10;
+        } else if (cardRank.equals("A")) { //If the hand total plus 11 doesn't make the player/dealer bust, then the value of the ace is 11
+            if (handValue + 11 <= 21) {
+                cardValue = 11;
+            } else {
+                cardValue = 1;
+            }
+        } else {
+            cardValue = Integer.valueOf(cardRank);
+        }
+        return cardValue;
+    }
+
+}
+
+class Card {
+    private String cardRank;
+
+    public void setCardRank(String cardVisual) { //Gets the rank of the card
+        cardRank = cardVisual.substring(0, cardVisual.indexOf("-"));
+    }
+
+    public String getCardRank() {
+        return cardRank;
+    }
+
 }
 
 class Deck {
@@ -240,6 +246,8 @@ class Deck {
     private String card;
     private int counter = 0;
     private boolean isEmpty = true;
+
+    private String randCardVisual;
 
     public void fillDeck() { //Fills deck with all cards
         counter = 0;
@@ -282,16 +290,27 @@ class Deck {
         }
     }
 
-    public void setRandCard() { //Getting an element number for the array from 1 to 52
-        elementNum = (int) (Math.random() * 52);
+    public void setRandCardVisual() { //Getting an element number for the array from 1 to 52
+
+        //Check if the deck is empty and fill
+        if (isDeckEmpty()) {
+            System.out.println("Deck is empty");
+            fillDeck();
+        }
+
+        //Loops until a card is found that hasn't been used
+        do {
+            elementNum = (int) (Math.random() * 52);
+        } while (deck[elementNum].equals("R"));
+        randCardVisual = deck[elementNum];
     }
 
-    public String getRandCard() { //Finds element in the deck and returns it
+    public String getRandCardVisual() { //Finds element in the deck and returns it
         //Check if the deck is empty and fill
         if (isDeckEmpty()) {
             fillDeck();
         }
-        return deck[elementNum];
+        return randCardVisual;
     }
 
     public boolean isDeckEmpty() { //Checks if the deck is empty
@@ -305,4 +324,11 @@ class Deck {
         }
         return isEmpty;
     }
+
+    public void displayDeck() {
+        for (int j = 0; j < deck.length; j++) {
+            System.out.print(deck[j] + "\t");
+        }
+    }
 }
+
